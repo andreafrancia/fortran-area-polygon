e97a780131536203f2e3d546798822fac8d1f72c
Andrea Francia <andrea@andreafrancia.it>
2016-08-19 17:03:53 +0200

    Works but with copy and paste of the function

--- test.f90
+++ test.f90
@@ -1,3 +1,73 @@
+FUNCTION polyarea(x, y, nb) RESULT(fn_val)
+ 
+! Code converted using TO_F90 by Alan Miller
+! Date: 2000-07-04  Time: 12:24:06
+
+IMPLICIT NONE
+
+REAL, INTENT(IN)     :: x(:)
+REAL, INTENT(IN)     :: y(:)
+INTEGER, INTENT(IN)  :: nb
+REAL                 :: fn_val
+
+!*****************************************************************
+
+!   GIVEN A SEQUENCE OF NB POINTS (X(I),Y(I)),  polyarea COMPUTES THE AREA
+! BOUNDED BY THE CLOSED POLYGONAL CURVE WHICH PASSES THROUGH THE POINTS IN
+! THE ORDER THAT THEY ARE INDEXED.  THE FINAL POINT OF THE CURVE IS ASSUMED
+! TO BE THE FIRST POINT GIVEN.  THEREFORE, IT NEED NOT BE LISTED AT THE END
+! OF X AND Y.  THE CURVE IS NOT REQUIRED TO BE SIMPLE.  e.g. It may cross over
+! itself.
+
+!*****************************************************************
+
+INTEGER  :: i, n, nm1
+REAL     :: a
+
+n = nb
+IF (x(1) == x(n) .AND. y(1) == y(n)) n = n - 1
+
+SELECT CASE (n)
+  CASE (:2)
+     fn_val = 0.0
+
+  CASE (3)
+     fn_val= 0.5*((x(2) - x(1))*(y(3) - y(1)) - (x(3) - x(1))*(y(2) - y(1)))
+
+  CASE DEFAULT
+    nm1 = n - 1
+    a = x(1)*(y(2) - y(n)) + x(n)*(y(1) - y(nm1))
+    DO  i = 2, nm1
+      a = a + x(i)*(y(i+1) - y(i-1))
+    END DO
+    fn_val = 0.5*a
+END SELECT
+
+RETURN
+END FUNCTION polyarea
+
+
+
 Program Hello
-Print *, "Hello World!"
+    interface
+         FUNCTION polyarea(x, y, nb) RESULT(fn_val)
+         REAL, INTENT(IN)     :: x(:)
+         REAL, INTENT(IN)     :: y(:)
+         INTEGER, INTENT(IN)  :: nb
+         REAL                 :: fn_val
+         end function polyarea
+    end interface
+    REAL x(5)
+    REAL y(5)
+    REAL area
+    x(1)=0
+    y(1)=0
+    x(2)=1
+    y(2)=0
+    x(3)=1
+    y(3)=1
+    x(4)=0
+    y(4)=1
+    area = polyarea(x,y,4)
+    write(*,*) area
 End Program Hello
